9437597b236552df552343868166f32ffed04cde
Eyob Guyo <eyobast@gmail.com>
2017-09-14 15:24:42 -0400

    jaws

--- config/connection.js
+++ config/connection.js
@@ -5,12 +5,12 @@
 // Dependencies
 var mysql = require('mysql');
 var Sequelize = require("sequelize");
-var connection;
+var sequelize;
 
 // Creates mySQL connection using Sequelize
 
 if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
+	sequelize = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 	var sequelize = new Sequelize("sunday", "root", "", {
 	  host: "localhost",
