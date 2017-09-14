1f980a6cf721f58ca0765ca20c186a273fec4aac
Eyob Guyo <eyobast@gmail.com>
2017-09-14 14:01:18 -0400

    push

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use(bodyParser.json());
  
 app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
  
-app.use(passport.initialize());
+// app.use(passport.initialize());
  
-app.use(passport.session()); // persistent login sessions
+// app.use(passport.session()); // persistent login sessions
 //For Handlebars
 app.set('views', './views')
 app.engine('hbs', exphbs({
